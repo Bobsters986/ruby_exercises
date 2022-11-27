8eeb6593cc84a0a884b8c4cc2a40835705581dd1
Bobby Luly <bobbyluly@gmail.com>
2022-11-26 18:46:26 -0700

    Add answers

---
+++ mythical-creatures/lib/dragon.rb
@@ -0,0 +1,25 @@
+class Dragon
+    attr_accessor :name, :color, :rider, :hungry, :num_meals
+
+    def initialize(name, color, rider)
+        @name = name
+        @color = color
+        @rider = rider
+        @hungry = true
+        @num_meals = 0
+    end
+
+    def eat
+        # @num_meals += 1
+        @num_meals += 1
+        @hungry = false if num_meals > 2
+    end
+
+    def hungry?
+        # if @num_meals > 2
+        #     @hungry = false
+        # else true
+        # end
+        hungry
+    end
+end
