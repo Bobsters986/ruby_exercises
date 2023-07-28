25f60cdd891cfd29ac9075cbee45764446cfb4af
Bobby Luly <bobbyluly@gmail.com>
2023-07-28 14:34:19 -0600

    Add methods to pass tests

--- objects-and-methods/exercise-2/spec/candy_spec.rb
+++ objects-and-methods/exercise-2/spec/candy_spec.rb
@@ -14,13 +14,13 @@ RSpec.describe Candy do
     expect(candy.type).to eq('Mars')
   end
 
-  xit 'has 100g of sugar by default' do
+  it 'has 100g of sugar by default' do
     candy = Candy.new('Smarties')
 
     expect(candy.sugar).to eq(100)
   end
 
-  xit 'can be created with a different amount of sugar'  do
+  it 'can be created with a different amount of sugar'  do
     candy = Candy.new('Pop Rocks', 75)
 
     expect(candy.sugar).to eq(75)
